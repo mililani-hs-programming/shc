0d4383517977c0577cbcf5be8e5406cd396c53d1
JadeEngstrom <jade_eng@yahoo.com>
2019-11-02 22:04:28 -0500

    11/2 POST Unix Shit

--- backend.py
+++ backend.py
@@ -2,23 +2,24 @@
 from datetime import datetime
 #unix_Converter()
 import MySQLdb
+import pdb
 #import sqlite3
 
 con = MySQLdb.connect(db="hacc",host="pf.parsl.dev", user="hacc", passwd="hacc2019")
 #con = sqlite3.connect('shc.db')
 cursorObj = con.cursor()
 
-def findMaxTime(con):
-    con.query("SELECT MIN([Start_Time]) FROM raw")
-    maxTimeResult = con.store_result()
-    maxTime = maxTimeResult.fetch_row(maxrows=0)
-    return(maxTime)
-
 def findMinTime(con):
-    con.query("SELECT MAX([End_Time]) FROM raw")
+    con.query("SELECT MIN(Start_Time) FROM raw")
     minTimeResult = con.store_result()
     minTime = minTimeResult.fetch_row(maxrows=0)
     return(minTime)
+
+def findMaxTime(con):
+    con.query("SELECT MAX(End_Time) FROM raw")
+    maxTimeResult = con.store_result()
+    maxTime = maxTimeResult.fetch_row(maxrows=0)
+    return(maxTime)
 
     '''max_time = cursorObj.execute("SELECT MAX([End_Time]) FROM raw")
     min_time =  cursorObj.execute("SELECT MIN([Start_Time]) FROM raw")'''
@@ -27,7 +28,7 @@ def findMinTime(con):
 
 def add_column(db, table, column, data, dtype="TEXT"):
     """Set a column equal to a list, creating the column if it doesn't exist"""
-
+    pdb.set_trace()
     # Sanity checks #
     if type(db) is not MySQLdb.connections.Connection:
         raise TypeError("Expected MySQLdb.connections.connection, got {}".format(type(db)))
@@ -48,7 +49,7 @@ def add_column(db, table, column, data, dtype="TEXT"):
 
     # Find max row num to know when to stop updating and start inserting #
     cur = db.cursor()
-    cur.execute("SELECT MAX(ID) FROM {}".format(table))
+    cur.execute("SELECT MAX(id) FROM {}".format(table))
     maxindex = cur.fetchall()
 
     # Create column and add in data #
@@ -60,6 +61,7 @@ def add_column(db, table, column, data, dtype="TEXT"):
         if i > maxindex[0][0]:  # Insert if row doesn't exist
             db.query("INSERT INTO {} ({}) VALUES ('{}')".format(table, column, value))
         i += 1
+
 #Unix Converter and stuff
 def sql_fetch_StartTimes(con):
     con.query('SELECT Start_Time FROM raw')
